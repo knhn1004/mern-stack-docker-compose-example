6541d71e68e4442e812e14c0078e9683b0280bb2
Chia-hong Chou <chia-hongchou@Oliver-2.local>
2020-11-05 11:57:36 +0800

    frontend and backend reverse proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/posts');
+      const fetched = await response.json();
+      const fetchedPosts = fetched.data;
+      setPosts(fetchedPosts);
+    };
+    fetchPosts();
+  }, []);
+  const [posts, setPosts] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
+        <p>docker compose workflow test</p>
         <a
           className="App-link"
-	  href="https://chunhongweb.com"
+          href="https://chunhongweb.com"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn More
         </a>
+        <ul>
+          {posts &&
+            posts.map(post => (
+              <li key={post._id}>
+                {post.title} - {post.author}
+              </li>
+            ))}
+        </ul>
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 8080
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // routes
 const postRouter = require('./src/routes/post.router')
@@ -14,8 +15,10 @@ app.use(
   })
 )
 
+app.use(cors())
+
 app.use(bodyParser.json())
-app.use('/posts', postRouter)
+app.use('/api/posts', postRouter)
 
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`)
